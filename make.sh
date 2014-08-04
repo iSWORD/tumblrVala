7e62ca321f4bc522d76958370d2d9962ca78eb2b
Muhannad Ajjan <IndianSword15@gmail.com>
2014-08-04 03:03:12 +0300

    added disable warnings

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-valac tumblrsdk.vala request-exception.vala request-handler.vala client.vala --pkg gee-1.0 
+valac tumblrsdk.vala request-exception.vala request-handler.vala client.vala --pkg gee-1.0 --disable-warnings
